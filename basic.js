c77590d46c943fa22570e4902b74d1ae9b2850e5
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-02 22:30:43 +0545

    function_in_js

---
+++ basic.js
@@ -0,0 +1,14 @@
+// //Array concatination
+
+// let books = ['Math', 'Science', 'Nepali'];
+// let books2 = ['Physics', 'Chemistry'];
+// let final_books = books.concat(books2);
+// document.write(final_books);
+
+
+// function
+adding = function add() {
+    document.write("Hello World");
+}
+
+adding();
